212c403f149d108e318db500e579cef5eca5c0d6
Tracy Torisky <mommers1@verizon.net>
2022-02-19 16:28:03 -0500

    update about me page

--- react-portfolio/src/components/AboutMe.js
+++ react-portfolio/src/components/AboutMe.js
@@ -3,17 +3,17 @@ import React from 'react';
 function AboutMe() {
   return <div className="body"><h1>About Me</h1>
   <img id="profileImage" src="https://picsum.photos/200"/><br/><br/>
-  About Me:
+ 
+<div class="container"></div>
+  I am a new developer looking forward to working with databases as a career.  I bring a background as a generalist who is able to quickly identify changing priorities.<br/><br/>
 
-  I am a new developer looking forward to working with databases as a career.  I bring a background as a generalist who is able to quickly identify changing priorities.
+  I am detail-oriented and looking for a position that I can use to help brng any company to a higher level of efficiency and productivity.  I excel at identyfying and solving problems.<br/><br/>  I have experience working on group projects.  
 
-  I am detail-oriented and looking for a position that I can use to help brng any company to a higher level of efficiency and productivity.  I excel at identyfying and solving problems.  I have experience working on group projects.  
+  I am able to use my creativity and innovation that will allow me to excel at working with databases to work with data that an employer can use.<br/><br/>
 
-  I am able to use my creativity and innovation that will allow me to excel at working with databases to work with data that an employer can use.
+  <h4>Technical Skillset:</h4>
 
-  Technical Skillset:
-
-  HTML, CSS, JavaScript, Node.js, MS Office Suite, Google Suite, Rise Vision, MailChimp, Ingeniux, Zoom, Box, Slate, Adobe Acrobat XI
+  HTML, CSS, JavaScript, SQL, MongoDB, React Node.js, MS Office Suite, Google Suite, Rise Vision, MailChimp, Ingeniux, Zoom, Box, Slate, Adobe Acrobat XI
   </div>;
 }
 
