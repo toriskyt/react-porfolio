a184fcbfea208b139acd2ce24e77a18bc4e1046c
Tracy Torisky <mommers1@verizon.net>
2022-02-19 17:05:09 -0500

    updated links and text

--- react-portfolio/src/components/AboutMe.js
+++ react-portfolio/src/components/AboutMe.js
@@ -7,9 +7,9 @@ function AboutMe() {
 <div class="container"></div>
   I am a new developer looking forward to working with databases as a career.  I bring a background as a generalist who is able to quickly identify changing priorities.<br/><br/>
 
-  I am detail-oriented and looking for a position that I can use to help brng any company to a higher level of efficiency and productivity.  I excel at identyfying and solving problems.<br/><br/>  I have experience working on group projects.  
+  I am detail-oriented and looking for a position that I can use to help bring any company to a higher level of efficiency and productivity.  I excel at identifying and solving problems.<br/><br/> 
 
-  I am able to use my creativity and innovation that will allow me to excel at working with databases to work with data that an employer can use.<br/><br/>
+  I am able to use creativity and innovation that allows me to excel at working with databases to find answers that bring meaning to the data.<br/><br/>
 
   <h4>Technical Skillset:</h4>
 
--- react-portfolio/src/components/Resume.js
+++ react-portfolio/src/components/Resume.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
 function Resume() {
-  return <div className="body"><h1>Resume</h1>
+  return <div className="body"><h2>Resume</h2>
   <img id="profileImage" src="https://picsum.photos/200"/><br/><br/>
-  <a id="resumeLink" target="_blank" href={"https://docs.google.com/document/d/1-YTTORpgZhwIvPn0Zyte_LfOEaOHqL-jB3uoSRs4_ms/edit"}>Resume</a><br/>
+  <a id="resumeLink" target="_blank" href={"https://docs.google.com/document/d/1-YTTORpgZhwIvPn0Zyte_LfOEaOHqL-jB3uoSRs4_ms/edit"}><h1>Download My Resume</h1></a><br/><br/>
+
+  <h1>Frontend Proficiencies</h1><br/>
+  <ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>JQuery</li>
+    <li>React</li>
+    <li>Bootstrap</li>
+  </ul>
+
+  <h1>Backend Proficiencies</h1>
+
+  <ul>
+    <li>Node</li>
+    <li>Express</li>
+    <li>MySQL</li>
+    <li>MongoDB, Mongoose</li>
+    <li>REST</li>
+    <li>GraphQL</li>
+  </ul>
+  
   </div>;
 }
+
+
+
+
+
+
+
 
 export default Resume;
