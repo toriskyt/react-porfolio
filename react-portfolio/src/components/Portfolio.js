5fe93a86d240d7ef94b6c5241b2e05e74be8f5f1
Tracy Torisky <mommers1@verizon.net>
2022-02-19 16:39:47 -0500

    updated links

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -5,7 +5,7 @@ function Portfolio() {
   return <div className='body'><h1>Portfolio</h1>
   <div className='imageContainer'>
     <div>Password Generator<br/>
-      <a target="_blank" href={"https://toriskyt.github.io/random-password-generator/"}>GitHub link</a><br/>
+      <a target="_blank" href={"https://github.com/toriskyt/random-password-generator"}>GitHub link</a><br/>
       <a target="_blank" href={"https://toriskyt.github.io/random-password-generator/"}>Deployed link</a><br/>
       </div>
     <img alt="passwordGenerator img" src="./assets/passwordGenerator.jpg"/> 
@@ -13,16 +13,16 @@ function Portfolio() {
 
   <div className='imageContainer'>
     <div>Refactoring Exercise <br/>
-      <a target="_blank" href="https://toriskyt.github.io/refactoring-html-project/">GitHub link</a><br/>
-      <a target="_blank" href={"https://toriskyt.github.io/random-password-generator/"}>Deployed link</a><br/>
+      <a target="_blank" href="https://github.com/toriskyt/refactoring-html-project">GitHub link</a><br/>
+      <a target="_blank" href={"https://toriskyt.github.io/refactoring-html-project/"}>Deployed link</a><br/>
       </div>
     <img alt="refactoring img" src="./assets/refactoringExercise.jpg"/> 
   </div>
 
   <div className='imageContainer'>
     <div>TixShare<br/>
-    <a target="_blank" href="https://toriskyt.github.io/project-one/">GitHub link</a><br/>
-    <a target="_blank" href={"https://toriskyt.github.io/random-password-generator/"}>Deployed link</a><br/>
+    <a target="_blank" href="https://github.com/toriskyt/project-one">GitHub link</a><br/>
+    <a target="_blank" href={"https://toriskyt.github.io/project-one/"}>Deployed link</a><br/>
     </div>
     <img alt="TixShare img" src="./assets/TixShare.png"/> 
   </div>
