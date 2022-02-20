58c6b07672c2ab77c16793253bfe318f2592e353
Tracy Torisky <mommers1@verizon.net>
2022-02-20 12:58:00 -0500

    debugged routes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,48 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import AboutMe from "./components/AboutMe"
 import Contact from "./components/Contact"
 import Resume from "./components/Resume"
 import Portfolio from "./components/Portfolio"
 import Footer from "./components/Footer"
+import { useState } from "react"
+// create a state
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  function renderPage() {
+    switch (currentPage) {
+      case "AboutMe": return <AboutMe></AboutMe>
+      case "Contact": return <Contact></Contact>
+      case "Resume": return <Resume></Resume>
+      case "Portfolio": return <Portfolio></Portfolio>
+      default:
+        break;
+    }
+  }
   return (
-    <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/" exact element={<AboutMe/>}/>
-        <Route path="/contact" exact element={<Contact/>}/>
-        <Route path="/resume" exact element={<Resume/>}/>
-        <Route path="/portfolio" exact element={<Portfolio/>}/>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <>
+    <NavBar/>
+
+    {renderPage() }
+    <Footer/> 
+    </>
   );
 }
 
 export default App;
+
+// 1. pass currentPage and setCurrentPage as props into the NavBar component
+// currentPage is just the string value, the prop will be the state variable currentPage={currentPage}, and setCurrentPage is the clickHandler function that the navbar links will have passed as their onClick function 
+
+// ex <Nav setCurrentPage={setCurrentPage} /> 
+
+// // in nav 
+// <aboutmelink
+//   onClick={setCurrentPage("AboutMe")}
+// >
+
+// </aboutmelink>
+
+// 2. Inside NavBar need to add onClick handler functions and just place in setCurrentPage as the function and pass in the string for that page that we want to render setCurrentPage("AboutMe")
